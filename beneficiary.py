e08d2dd41a71510b93440ba6c8bcfa246bdcc77b
Kenneth Luna <kl1771@nyu.edu>
2015-04-21 17:45:23 -0400

    compound beneficiary files

---
+++ beneficiary.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import pandas as pd
+
+file_list = []
+
+def getFiles():
+	for i in os.listdir('/home/cusp/kl1771/cms_files/'):
+		if 'Beneficiary_Summary_File' in i:
+			file_list.append(i)
+		else:
+			pass
+
+	return file_list
+
+def mergePatients(list):
+	index = 0
+	for file in list:
+                if index == 0:
+                        df_temp1 = pd.read_csv(list[index])
+                        index += 1
+                elif index == 1:
+                        df_temp2 = pd.read_csv(list[index])
+                        df = pd.concat([df_temp1, df_temp2])
+                        index += 1
+                else:
+                        df_add = pd.read_csv(list[index])
+                        df = pd.concat([df, df_add])
+                        index += 1
+			print "adding the %sth index" % index
+	return df
+
+def compoundPatients(df):
+	col_diag = ['SP_ALZHDMTA', 'SP_CHF', 'SP_CHRNKIDN', 'SP_CNCR', 'SP_COPD', 'SP_DEPRESSN', 'SP_DIABETES', 'SP_ISCHMCHT', 'SP_OSTEOPRS', 'SP_RA_OA', 'SP_STRKETIA']
+	df_ben_grouped = df.groupby(['DESYNPUF_ID'])[col_diag]
+	df_ben_summary = df_ben_grouped.agg('median')
+	pd.DataFrame(df_ben_summary).to_csv("patient_summary.csv")
+
+if __name__=='__main__':
+	files = getFiles()
+	df = mergePatients(files)
+	compoundPatients(df)
